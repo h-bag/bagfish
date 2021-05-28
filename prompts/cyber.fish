ba5c9e796d6f376bb426eb65b3ac6eb7485aaa34
Nice Try, Officer <baguette@sdf.org>
2021-05-28 14:28:26 +0100

    Sets up a very basic prompt to test out a couple features.

---
+++ prompts/cyber.fish
@@ -0,0 +1,5 @@
+function fish_prompt --description 'A shiny, futuristic prompt.'
+         set_color -b 00FF00;
+         set_color FFFFFF; echo "test prompt best prompt"
+         set_color normal; echo " awooga >"
+end
